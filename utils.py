13a3655c48d220cb91ac0b152473687ef454a23c
Ole August <oleastole@gmail.com>
2022-10-11 21:19:21 +0200

    feat: implement working CNN

---
+++ utils.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+
+COLOR_MAP = 'YlGnBu'
+
+def display_images(images: list):
+    if len(images) == 1:
+        plt.imshow(images[0], cmap=COLOR_MAP)
+        return
+
+    fig, axs = plt.subplots(1, len(images), figsize=(12, 12))
+    for i in range(len(images)):
+        axs[i].axis('off')
+        axs[i].imshow(images[i], cmap=COLOR_MAP)
+        axs[i].title.set_text(f"Image {i+1}")
+        for j in range(images[i].shape[0]):
+            for k in range(images[i].shape[1]):
+                axs[i].text(k, j, f"{images[i][j,k]:.2f}", fontsize="small", ha="center", va="center")
+    
+    plt.show()
